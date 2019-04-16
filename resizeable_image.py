e636c67acb760f9042a96715909a8e759277d43d
wasobi <sierraobi@my.uri.edu>
2019-04-15 23:13:54 -0400

    add three cases

--- resizeable_image.py
+++ resizeable_image.py
@@ -1,5 +1,6 @@
 import imagematrix
 from array import *
+import math
 
 class ResizeableImage(imagematrix.ImageMatrix):
     def best_seam(self, dp=True):
@@ -14,10 +15,22 @@ class ResizeableImage(imagematrix.ImageMatrix):
         self.remove_seam(self.best_seam())
     	
     
-    def dynamic (self):
+    def dynamic (self,seam):
+    inf = math.inf
+    
+    _e_seam = []
+    i = 2 # start at the second row
     		# energy of the pixels on the edge of the image will always be 10000
     	for i in range (self.height):
     		for j in range (self.width):
+    		#calculate the energy seam at the current pixel
+    		if j == 0:
+    			_e_seam = min(inf,j,j+1)
+    		elif j == self.width - 1:
+    			_e_seam = min(j-1,j,inf)
+    		else:
+    			_e_seam = min(j-1,j,j+1)
+    		
     		# compute gradient at every pixel
     		# create energy map
     		# identify the lowest energy seam
@@ -48,3 +61,5 @@ class ResizeableImage(imagematrix.ImageMatrix):
     		return b
     	else:
     		return c
+
+
